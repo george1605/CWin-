1c7923e34ba18eaa7156144d106e63038c99c944
george1605 <32997165+george1605@users.noreply.github.com>
2021-04-27 11:14:22 +0300

    Update MAIN.h

--- MAIN.h
+++ MAIN.h
@@ -1,7 +1,10 @@
 #ifdef _WIN32_ 
   #include "Windows/CWin++.h"
+  #define _MAIN WinMain
 #elif _WIN64_
   #include "Windows/CWin++.h"
+  #define _MAIN WinMain
 #else
    #include "Qt/version 1/CWin++.h"
+   #define _MAIN int main()
 #endif
